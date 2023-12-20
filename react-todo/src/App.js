6220e128f9a66981363d3b3b93ec1fe79749d1db
D4L4stY4suo <lastyasuohasagi@gmail.com>
2023-12-20 13:17:20 +0100

    Create a Second React Component TodoTable

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import TodoRowItem from "./components/TodoRowItem";
+import TodoTable from "./components/TodoTable";
 
 function App() {
   const todos = [
@@ -18,6 +18,11 @@ function App() {
       rowDescription: "Make dinner",
       rowAssigned: "Kyuubi",
     },
+    {
+      rowNumber: 4,
+      rowDescription: "Do some stuffs",
+      rowAssigned: "Zed",
+    },
   ];
 
   return (
@@ -25,32 +30,7 @@ function App() {
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}
-              />
-            </tbody>
-          </table>
+          <TodoTable todos={todos}/>
         </div>
       </div>
     </div>
