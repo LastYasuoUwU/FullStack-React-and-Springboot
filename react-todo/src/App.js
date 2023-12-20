8fdeba9a001db15619b788f1457f9326fe3200a8
D4L4stY4suo <lastyasuohasagi@gmail.com>
2023-12-20 13:00:32 +0100

    create react app and a table for todo

---
+++ react-todo/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import TodoRowItem from "./components/TodoRowItem";
+
+function App() {
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem />
+              <tr>
+                <th scopre="row">2</th>
+                <td>Play some games</td>
+                <td>bel-dono</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-todo/src/components/TodoRowItem.js
@@ -0,0 +1,11 @@
+function TodoRowItem(params) {
+  return (
+    <tr>
+      <th scopre="row">1</th>
+      <td>Learn react fundamentals</td>
+      <td>bel-dono</td>
+    </tr>
+  );
+}
+
+export default TodoRowItem;
