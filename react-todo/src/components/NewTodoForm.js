f651afd411d6def1c4b155a0b04fb3c670f81f02
D4L4stY4suo <lastyasuohasagi@gmail.com>
2023-12-20 14:43:57 +0100

    Adding User Input to React Form

--- react-todo/src/components/NewTodoForm.js
+++ react-todo/src/components/NewTodoForm.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTodoForm(params) {
-    return(
-        <div className="mt-5">
-            <form>
-                <div className="mb-3">
-                    <label className="form-label">Assigned</label>
-                    <input type="text" className="form-control" required></input>
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Description</label>
-                    <textarea className="form-control" rows={3} required></textarea>
-                </div>
-                <button type="button" className="btn btn-primary mt-3">Add Todo</button>
-            </form>
+function NewTodoForm() {
+  const [description, setDescritpion] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  //   const descritpionChange = (event) => {
+  //     console.log("description: ", event.target.value);
+  //     setDescritpion(event.target.value);
+  //   };
+
+  //   const assignedChange = (event) => {
+  //     console.log("assigned: ", event.target.value);
+  //     setAssigned(event.target.value);
+  //   };
+
+  return (
+    <div className="mt-5">
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Assigned</label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={(e) => setAssigned(e.target.value)}
+            value={assigned}
+          ></input>
         </div>
-    )
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            required
+            onChange={(e) => setDescritpion(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <button type="button" className="btn btn-primary mt-3">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewTodoForm;
