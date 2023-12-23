9e0b33e8e5f9b45be71b062a4a69969a91d209a4
D4L4stY4suo <lastyasuohasagi@gmail.com>
2023-12-23 14:51:02 +0100

    setup library app FE

---
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the fullstack course :-D</h1>
+    </div>
+  );
+}
+
+export default App;
